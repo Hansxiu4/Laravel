8da455bc926400651c2902b6bd9fdc32a2fcff35
Farhan <hansxiu4@gmail.com>
2024-12-04 10:59:28 +0700

    Add files via upload

---
+++ data_santri.blade.php
@@ -0,0 +1,35 @@
+<h3>Data Santri</h3>
+<ol>
+    <table border>
+        <tr>
+            <th>Nomor</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+        </tr>
+    <tr>
+         <td>1</td> 
+          <td> {{ $mhs1 }} </td>
+         <td> {{ $asal1 }} </td>
+        </tr>
+    <tr>
+         <td>2</td> 
+          <td> {{ $mhs2 }} </td>
+         <td> {{ $asal2 }} </td>
+        </tr>
+    <tr>
+         <td>3</td> 
+          <td> {{ $mhs3 }} </td>
+         <td> {{ $asal3 }} </td>
+        </tr>
+    <tr>
+         <td>4</td> 
+          <td> {{ $mhs4 }} </td>
+         <td> {{ $asal4 }} </td>
+        </tr>
+    <tr>
+         <td>5</td> 
+          <td> {{ $mhs5 }} </td>
+         <td> {{ $asal5 }} </td>
+        </tr>
+    </table>
+</ol>
